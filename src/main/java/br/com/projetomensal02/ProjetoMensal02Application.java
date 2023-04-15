b2e374f1c6b96f62942fb3c712c364ba02ffd996
Lucaslmp77 <lucas.lmp77@gmail.com>
2023-04-15 20:05:46 -0300

    Iniciar projeto em Spring Boot e configurar Swagger #1

---
+++ src/main/java/br/com/projetomensal02/ProjetoMensal02Application.java
@@ -0,0 +1,13 @@
+package br.com.projetomensal02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMensal02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMensal02Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projetomensal02/ProjetoMensal02ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetomensal02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMensal02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

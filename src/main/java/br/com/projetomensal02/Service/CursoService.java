81ec1b786298b54cad78825f344876ae369a00bc
Lucaslmp77 <lucas.lmp77@gmail.com>
2023-04-16 15:18:33 -0300

    #13 - Criar métodos cadastrar, editar, remover, listar, buscar curso por sigla e buscar curso por nome da classe Curso.#13

--- src/main/java/br/com/projetomensal02/Service/CursoService.java
+++ src/main/java/br/com/projetomensal02/Service/CursoService.java
@@ -1,11 +1,54 @@
 package br.com.projetomensal02.Service;
 
-import br.com.projetomensal02.Repository.AlunoRepository;
+import br.com.projetomensal02.Entity.Curso;
+import br.com.projetomensal02.Repository.CursoRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CursoService {
     @Autowired
-    private AlunoRepository alunoRepository;
+    private CursoRepository cursoRepository;
+
+    @Transactional
+    public Curso save(Curso curso) {
+        return this.cursoRepository.save(curso);
+    }
+
+    public List<Curso> listAll() {
+        return this.cursoRepository.findAll();
+    }
+
+    public Curso findById(Long id) {
+        return this.cursoRepository.findById(id).orElse(new Curso());
+    }
+
+    @Transactional
+    public void update(Long id, Curso curso) {
+        if(id == curso.getId()) {
+            this.cursoRepository.save(curso);
+        } else {
+            throw new RuntimeException();
+        }
+    }
+
+    @Transactional
+    public void delete(Long id, Curso curso) {
+        if(id == curso.getId()) {
+            this.cursoRepository.delete(curso);
+        } else {
+            throw new RuntimeException();
+        }
+    }
+
+    public List<Curso> findCursoBySigla(String sigla) {
+        return this.cursoRepository.findCursoBySigla(sigla);
+    }
+
+    public List<Curso> findCursoByNome(String nome) {
+        return this.cursoRepository.findCursoByNome(nome);
+    }
 }

9ee0fff17abe94a0f39f2387c41417c79a1b3a7c
Lucaslmp77 <lucas.lmp77@gmail.com>
2023-04-16 17:51:45 -0300

    #15 - Correções de erros referente a classe Turma na camada controller.#15

--- src/main/java/br/com/projetomensal02/Controller/ProfessorController.java
+++ src/main/java/br/com/projetomensal02/Controller/ProfessorController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/api/professor")
 public class ProfessorController {

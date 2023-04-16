7db046267de2c5b5f7723c8ec4148ea692f82f49
Renanmontanopaz <renanmontanopaz@outlook.com>
2023-04-16 15:49:15 -0300

    Services feitas

--- src/main/java/br/com/projetomensal02/Service/ProfessorService.java
+++ src/main/java/br/com/projetomensal02/Service/ProfessorService.java
@@ -1,11 +1,61 @@
 package br.com.projetomensal02.Service;
 
-import br.com.projetomensal02.Repository.AlunoRepository;
+import br.com.projetomensal02.Entity.Professor;
+import br.com.projetomensal02.Repository.ProfessorRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.List;
 
 @Service
 public class ProfessorService {
     @Autowired
-    private AlunoRepository alunoRepository;
+    private ProfessorRepository professorRepository;
+
+    public List<Professor> listAll(){ return this.professorRepository.findAll(); }
+
+    public List<Professor> findProfessoresByNomeCurso(String nomeCurso){ return this.professorRepository.findProfessoresByNomeCurso(nomeCurso); }
+
+    public List<Professor> findProfessoresByIdTurma(Long idTurma){ return this.professorRepository.findProfessoresByIdTurma(idTurma); }
+
+    public List<Professor> findProfessoresByNome(String nomeProfessor){ return this.professorRepository.findProfessoresByNome(nomeProfessor); }
+
+    public List<Professor> findProfessoresByEspecialidade(String nomeEspecialidade){ return this.professorRepository.findProfessoresByEspecialidade(nomeEspecialidade); }
+
+    public Professor findById(Long id) { return this.professorRepository.findById(id).orElse(new Professor());}
+
+    @Transactional
+    public Professor save(Professor professor){
+        Assert.isTrue(checkCamposIsNull(professor), "Erro, algum campo do professor é nulo");
+        return this.professorRepository.save(professor);
+    }
+
+    public boolean checkCamposIsNull(Professor professor){
+        if(professor.getNome()==null || professor.getEndereco()==null || professor.getEspecialidade()==null){
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    @Transactional
+    public void update(Long id, Professor professor){
+        if (id==professor.getId()){
+            this.professorRepository.save(professor);
+        }else {
+            throw new RuntimeException();
+        }
+    }
+
+    @Transactional
+    public void remove(Long id, Professor professor){
+        if (id==professor.getId()){
+            this.professorRepository.delete(professor);
+        } else {
+            throw new RuntimeException();
+        }
+    }
+
 }

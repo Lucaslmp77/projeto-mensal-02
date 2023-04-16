f5a538f5371305be7928fedd71a50b3423a9cfa2
Lucaslmp77 <lucas.lmp77@gmail.com>
2023-04-16 13:52:08 -0300

    #9 - Buscar curso por sigla, buscar curso por nome.#9

--- src/main/java/br/com/projetomensal02/Repository/CursoRepository.java
+++ src/main/java/br/com/projetomensal02/Repository/CursoRepository.java
@@ -2,9 +2,17 @@ package br.com.projetomensal02.Repository;
 
 import br.com.projetomensal02.Entity.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CursoRepository extends JpaRepository<Curso, Long> {
+    @Query("FROM Curso curso WHERE curso.sigla = :sigla")
+    public List<Curso> findCursoBySigla(@Param("sigla") String sigla);
 
+    @Query("FROM Curso curso WHERE curso.nome = :nome")
+    public List<Curso> findCursoByNome(@Param("nome") String nome);
 }

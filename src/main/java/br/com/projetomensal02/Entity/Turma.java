dd2f414ea0515791b95dcd9f0f5836f14b30ce1f
Lucaslmp77 <lucas.lmp77@gmail.com>
2023-04-15 22:24:02 -0300

    #3 - Criar classes "Professor, Turma, Curso, Aluno" e seus atributos no package "Entity".

---
+++ src/main/java/br/com/projetomensal02/Entity/Turma.java
@@ -0,0 +1,30 @@
+package br.com.projetomensal02.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_turma", schema = "projeto-mensal-02")
+public class Turma extends AbstractEntity {
+    @Getter
+    @Setter
+    @Column(name = "semestre", length = 10, nullable = false)
+    private Integer semestre;
+
+    @Getter @Setter
+    @Column(name = "ano", length = 10, nullable = false)
+    private Integer ano;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name="id_aluno")
+    private Aluno aluno;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name="id_professor")
+    private Professor professor;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name="id_curso")
+    private Curso curso;
+}

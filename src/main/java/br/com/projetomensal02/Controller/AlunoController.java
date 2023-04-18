cb710aa7c504ba7d0b670d2184b0c02cd67a5190
Lucaslmp77 <lucas.lmp77@gmail.com>
2023-04-18 00:57:37 -0300

    #1 - Correções na configuração do swagger. #1

--- src/main/java/br/com/projetomensal02/Controller/AlunoController.java
+++ src/main/java/br/com/projetomensal02/Controller/AlunoController.java
@@ -4,8 +4,9 @@ import br.com.projetomensal02.Service.AlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/aluno")
 public class AlunoController {
     @Autowired

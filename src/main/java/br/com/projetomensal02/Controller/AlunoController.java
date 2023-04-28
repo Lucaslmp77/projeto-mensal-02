8a0e0dd127a119180d4324eeed834f148d53c0f9
Lucaslmp77 <lucas.lmp77@gmail.com>
2023-04-28 12:44:14 -0300

    Annotation Controller alterada para @RestController, para utilização no Swagger.

--- src/main/java/br/com/projetomensal02/Controller/AlunoController.java
+++ src/main/java/br/com/projetomensal02/Controller/AlunoController.java
@@ -4,14 +4,13 @@ import br.com.projetomensal02.Entity.Aluno;
 import br.com.projetomensal02.Service.AlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api/aluno")
 public class AlunoController {
     @Autowired

07e4a27deaf53d62a3b492f66b1280fac0f2acd0
Lucaslmp77 <lucas.lmp77@gmail.com>
2023-05-02 15:12:29 -0300

    Teste unitario impedindo o usuario de cadastrar um curso com o nome composto por numeros.

--- src/main/java/br/com/projetomensal02/Entity/Curso.java
+++ src/main/java/br/com/projetomensal02/Entity/Curso.java
@@ -34,6 +34,8 @@ public class Curso extends AbstractEntity {
             throw new RuntimeException("O nome do curso inserido é muito curto");
         } else if (nome.trim().length() > 25) {
             throw new RuntimeException("O nome do curso inserido ultrapassa o limite máximo");
+        } else if (nome.matches("[0-9]+")) {
+            throw new RuntimeException("O nome do curso inserido é composto por números");
         } else {
             this.nome = nome;
         }
